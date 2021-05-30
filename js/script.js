466118de72f201ec5161103768be6d3bfe2b6a50
Mateusz Fibich <mateusz.fibich@gmail.com>
2021-05-30 23:01:50 +0200

    Added script js

---
+++ js/script.js
@@ -0,0 +1,86 @@
+{
+    const tasks = [];
+
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+          <li class= "list__item js-tasks">
+          <button class="list__button js-done" >
+          ${task.done ? "✔" : ""}
+          </button>
+          <span class= "${task.done ? "list__task--done" : ""}">${task.content}</span>
+          <button class=" list__buttonRemove js-remove">🗑</button>
+          </li>
+          `;
+
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
+
+    };
+
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+        }
+
+        document.querySelector(".js-newTask").value = "";
+    };
+
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
